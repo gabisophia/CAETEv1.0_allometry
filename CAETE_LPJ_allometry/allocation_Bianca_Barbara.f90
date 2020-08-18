98b7e8d6fb5d4ae049faf476a527bb0d72ab5158
Bárbara Cardeli <barbara.r.cardeli@gmail.com>
2020-08-18 18:16:58 -0300

    Joining the functions bisection_method and X. Not finished!

--- CAETE_LPJ_allometry/allocation_Bianca_Barbara.f90
+++ CAETE_LPJ_allometry/allocation_Bianca_Barbara.f90
@@ -25,9 +25,6 @@ program allocation
     real :: SS = 2 !SOMENTE PARA TESTES
     real :: R = 4 !SOMENTE PARA TESTES
     real :: SW = 5 !SOMENTE PARA TESTES
-    real :: x !SOMENTE PARA TESTES
-    real :: a = 0.0
-    real :: b = 3.0
     real :: midpoint
     real :: Delta_R
     real :: Delta_S
@@ -42,28 +39,24 @@ program allocation
     real :: result_tau3
     real, external :: sapwood
     real :: result_SS
-    real, external :: f 
-    real :: result_f
     real, external :: bisection_method
     real :: Delta_L
     
     result_tau1 = calc_tau1(k_allom2, k_allom3, dw)
-    print*, 'result_tau1 =', result_tau1
+    !print*, 'result_tau1 =', result_tau1
     
     result_tau2 = calc_tau2(k_allom3)
-    print*, 'result_tau2 =', result_tau2
+    !print*, 'result_tau2 =', result_tau2
 
     result_tau3 = calc_tau3(klatosa, dw, spec_leaf)
-    print *, "result_tau3 = ", result_tau3
+    !print *, "result_tau3 = ", result_tau3
 
     result_SS = sapwood(ltor, R, SW, L, bminc)
-    print *, 'result_SS =', result_SS
-
-    result_f = f(x, result_tau1, result_tau2, result_tau3, ltor, H, L, SS)
-    print *, 'SEARCH X (func. F)=', result_f
+    !print *, 'result_SS =', result_SS
+    
 
 ! Main Code: Use the bisection method to solve for the leaf mass increment
-    Delta_L = bisection_method(a, b, tol, midpoint)
+    Delta_L = bisection_method(0.0, 3.0)
     print *, 'DELTA L =', Delta_L
 
 !Once we have the leaf mass increment we can cant get root mass increment based on the LTOR constant
@@ -130,26 +123,33 @@ end function sapwood
 !Minimization function with delta.leaf as x
 !A value of x is searched with f(x) = 0
 
-!FUNÇÃO 'def f(self, x)' DO CÓDIGO DO PHILIP UTILIZANDO VALORES ALEATÓRIOS PARA 'H', 'L' e 'SS' > para testes.
-
-function f (x, result_tau1, result_tau2, result_tau3, ltor, H, L, result_SS) result (searched_x)
-    implicit none
-    real, intent(in) :: x
-    real, intent(in) :: result_tau1
-    real, intent(in) :: result_tau2
-    real, intent(in) :: result_tau3
-    real, intent(in) :: ltor
-    real, intent(in) :: H
-    real, intent(in) :: L 
-    real, intent(in) :: result_SS
-    real :: searched_x
-
-    return
-    searched_x = result_tau1 * (result_SS - x - x / ltor + H) - ((result_SS - x - x / ltor) /&
-     &(L + x) * result_tau3) ** result_tau2
- 
-end function f
 !===========================================================================!
+
+!function caneta() result (result_caneta)
+    !implicit none
+     
+    !real :: result_caneta
+    !real :: result_lapis
+
+    !result_caneta = 200
+
+
+    !return
+    
+    !contains
+
+    !function lapis() result (result_lapis)
+        !implicit none
+        !real :: result_lapis
+        !real :: result_caneta
+
+        !result_lapis = result_caneta + 200
+
+        !return
+    !end function lapis
+
+!end function caneta
+    
 
 !===========================================================================!
 !Numerical root-finding method to find a solution x with f(x) = 0
@@ -157,38 +157,54 @@ end function f
 
 !FUNÇÃO DO BISECTION_METHOD >>>> Conferir se está correta com a lógica do Philip em Python.
 
-function bisection_method (a, b, tol, midpoint) result (Delta_L)
+function bisection_method (a, b) result (Delta_L)
     implicit none
-    real :: a
-    real :: b
-    real, intent(in) :: tol
+    real :: a, b
+    real :: tol
     real :: midpoint
     real :: Delta_L
 
-    if ((a * b) .gt. 0) then 
+    if ((f(a) * f(b)) .gt. 0) then 
+        Delta_L = -2.0 
         return 
-            !a * b = -2.0 !VERIFICAR PQ TÁ DANDO ERRADO NO 'a': Unclassifiable statement at (1)
     endif 
 
     if ((b - a) / 2.0 .gt. tol) then
         midpoint = (a + b) / 2.0
-        elseif (midpoint .gt. 0) then
+        elseif (f(midpoint) .gt. 0) then
             return 
     endif 
 
-    if ((a * midpoint) .lt. 0) then
+    if ((f(a) * f(midpoint) .lt. 0)) then
         b = midpoint
     else 
         a = midpoint
     endif 
+
+    return 
+
+    contains
+    function f (x) result (searched_x)
+        implicit none
+
+        real, intent(in) :: x
+        real :: searched_x
+        real :: result_tau1
+        real :: result_tau2
+        real :: result_tau3
+        real :: result_SS
+        real :: ltor 
+        real :: H 
+        real :: L
+
+        searched_x = x + 100 + a !Para testes
+        print*, 'RESULTADOOO', searched_x
+        
+        !searched_x = result_tau1 * (result_SS - x - x / ltor + H) - ((result_SS - x - x / ltor) /&
+         !&(L + x) * result_tau3) ** result_tau2
+     
+        return
+    end function f
          
 end function bisection_method
 !===========================================================================!
-
-! function funcao_que_usa_resultado_de_outra(resultado_da_outra) result(novo_resultado)
-!     implicit none
-!     real, intent(in) :: resultado_da_outra
-!     real :: novo_resultado
-
-!     novo_resultado = resultado_da_outra * 2
-! end function funcao_que_usa_resultado_de_outra
